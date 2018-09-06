8e28da0a65651e5af88cdc5b7cd4969038909edd
Kris Jenkins <krisajenkins@gmail.com>
2018-09-06 12:57:02 +0100

    Adding a versions file to the hydra build targets.

--- jobsets/release-testnet-healthcheck.nix
+++ jobsets/release-testnet-healthcheck.nix
@@ -1,8 +1,32 @@
 { nixpkgs ? <nixpkgs>
 , testnetHealthcheckSrc ? ./..
 }:
-let pkgs = import nixpkgs {};
-in {
+let
+  pkgs = import nixpkgs {};
+
+  listVersions = derivations: pkgs.stdenv.mkDerivation {
+    name = "versions";
+    src = ./.;
+    buildInputs = [ pkgs.git ];
+    installPhase =
+      builtins.concatStringsSep "\n"
+        (
+          [ "mkdir $out" ]
+          ++
+          (builtins.map
+            (derivation: ''
+                (
+                  cd ${derivation.src}
+                  SHA=`git rev-parse HEAD || echo UNKNOWN`
+                  printf "%40s %s\n" $SHA ${derivation.name}
+                ) >> $out/versions.txt
+             '')
+             derivations)
+        );
+  };
+in rec {
+  versions = listVersions [ testnetHealthcheckClient testnetHealthcheckServer ];
+
   testnetHealthcheckClient = pkgs.callPackage ../client {
     inherit testnetHealthcheckSrc;
   };
