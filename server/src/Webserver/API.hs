a2ad7a642119d2e5963bcaad6be989256009ace9
Kris Jenkins <krisajenkins@gmail.com>
2018-09-17 13:10:38 +0100

    Improving our hindent setup for servant, and reformatting.

--- server/src/Webserver/API.hs
+++ server/src/Webserver/API.hs
@@ -1,20 +1,15 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE DataKinds #-}
 
--- | hindent doesn't really play well with Servant's type operators, so
--- | just convenience we put the API spec in its own module, where we
--- | can avoid autoformatting.
 module Webserver.API
   ( Web
   ) where
 
 import Data.Text (Text)
-import Servant (Raw, (:<|>), (:>), Get, PlainText)
+import Servant ((:<|>), (:>), Get, PlainText, Raw)
 import Servant.API.WebSocket (WebSocket)
 
-type Web =
-  "version" :> Get '[ PlainText] Text
-  :<|>
-  "ws" :> WebSocket
-  :<|>
-  Raw
+type Web
+   = "version" :> Get '[ PlainText] Text
+     :<|> "ws" :> WebSocket
+     :<|> Raw
